6916cc174c932dd9685525bfcdf20bf9fbd781ae
farrux <farrux.avganov@mail.ru>
2022-05-31 21:55:58 +0300

    finish priject

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-// const openBtn = document.querySelector(".banner__btn");
-// const modal = document.querySelector(".modal");
-// const closeBtn = document.querySelector(".modal__btn");
+const openBtn = document.querySelector(".banner__btn");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".modal__btn");
 
 
+openBtn.addEventListener("click", function () {
+	modal.classList.add("show");
+})
+
+closeBtn.addEventListener("click", function () {
+	modal.classList.remove("show");
+})
